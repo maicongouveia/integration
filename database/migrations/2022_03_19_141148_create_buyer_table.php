95f5688a23bd33784264d8792fe749f2eabb7432
Maicon Gouveia <gouveia.maicon@gmail.com>
2022-03-19 11:33:10 -0300

    Creating migrations

---
+++ database/migrations/2022_03_19_141148_create_buyer_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateBuyerTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create(
+            'buyer', 
+            function (Blueprint $table) {
+                $table->id();
+                $table->unsignedBigInteger('order_id'); 
+                $table->foreign('order_id')->references('id')->on('order');
+                $table->string('name');
+                $table->string('email');
+                $table->string('identificationType');
+                $table->string('identificationNumber');
+                $table->string('phone');
+                $table->timestamps();
+            }
+        );
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('buyer');
+    }
+}
